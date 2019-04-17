905ec7ce9b6b85f33de2646ab90a3d32e878cb25
DasSonia <soniadas363@gmail.com>
2019-04-17 09:57:54 +0530

    Variable Declaration

--- src/Hello.java
+++ src/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
 	// Entry point for our program
 	public static void main(String[] args) {
 		System.out.println("Hello World!");		
+		int FirstNumber = 10;
+		System.out.println(FirstNumber);		
 	} 
 }
